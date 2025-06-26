800303d215e6c353308fd80ad766f2c5bc2715c5
kruthika-reddy7 <156561723+kruthika-reddy7@users.noreply.github.com>
2025-06-26 10:57:10 +0530

    Create todolist.js
    in components

---
+++ todolist.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <div>
+      {todos.map((todo, index) => (
+        <TodoItem
+          key={index}
+          index={index}
+          todo={todo}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default TodoList;
